d1caa3be6ce816c61a4ac6cef5964d2fe1981318
ldmachad <leonardodiomachado@gmail.com>
2023-10-10 17:09:24 -0300

    Update AppDbContext

--- blogpessoal/Data/AppDbContext.cs
+++ blogpessoal/Data/AppDbContext.cs
@@ -44,7 +44,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.UtcNow);
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
@@ -57,7 +57,7 @@ namespace blogpessoal.Data
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.UtcNow);
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
